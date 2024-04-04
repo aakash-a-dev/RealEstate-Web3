e71950a1ac9754224ac5548323ebbc78f132b83c
Aakash Amod Rajput <126223325+aakash-a-dev@users.noreply.github.com>
2024-04-04 23:36:01 +0530

    Settled RealEstate.sol

--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -1,6 +1,17 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
+import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract RealEstate {
+contract RealEstate is ERC721URIStorage {
+    uint256 private _nextTokenId;
 
+    constructor() ERC721("Real Estate", "REAL") {}
+
+    function mint(string memory tokenURI) public returns (uint256) {
+        uint256 tokenId = _nextTokenId++;
+        _mint(msg.sender, tokenId);
+        _setTokenURI(tokenId, tokenURI);
+
+        return tokenId;
+    }
 }
